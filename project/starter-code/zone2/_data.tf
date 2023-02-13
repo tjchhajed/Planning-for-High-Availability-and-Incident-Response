aa96f7bd6e15e33a4857df4dea9e051ee5b890db
Tejashree Chhajed <teju@Surajits-MBP.fritz.box>
2023-02-13 08:03:20 +0100

    reverted data.tf modifications

--- project/starter-code/zone2/_data.tf
+++ project/starter-code/zone2/_data.tf
@@ -19,9 +19,9 @@ data "aws_caller_identity" "current" {}
 data "terraform_remote_state" "vpc" {
   backend = "s3" 
   config = {
-     bucket = "udacity-tf-tejashree-west"
+     bucket = "udacity-tf-tejashree"
      key    = "terraform/terraform.tfstate"
-     region = "us-west-1"
+     region = "us-east-2"
    }
 
 
